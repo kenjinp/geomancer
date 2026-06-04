17826426921875b14dabe04037956018d64d39be
Kenny Pirman <kenny.pirman@character.ai>
2026-06-03 21:40:45 -0400

    fix: upgrade web infra dependencies

--- infra/pulumi/index.ts
+++ infra/pulumi/index.ts
@@ -1,7 +1,7 @@
 import * as aws from "@pulumi/aws";
 import * as pulumi from "@pulumi/pulumi";
 import * as fs from "fs";
-import * as mime from "mime";
+import mime from "mime";
 import * as path from "path";
 
 // Import the program's configuration settings.
